12661581c9d06c84705f987b14a78d3e150469a6
Ben Thacher <8000ice@gmail.com>
2024-05-02 12:56:29 -0400

    Fixed build issues

--- setup.sh
+++ setup.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-# Add pal-engine submodule if it doesn't exist
-if [ ! -d pal-engine ]; then
-    git submodule add git@github.com:benthacher/pal-engine.git
+if git submodule status | grep --quiet '^-'; then
+    git submodule add -b main git@github.com:benthacher/pal-engine.git
+fi
+
+if [ ! -d "./pal-engine" ]; then
+    git submodule update --init --recursive
 fi
 
 # Make sure pal-engine is up to date
-git submodule update --remote
+git submodule update --remote --recursive
 
 # Run setup script for pal-engine
 cd ./pal-engine/util
